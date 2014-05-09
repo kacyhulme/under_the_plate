e335e738ce16e57bd0dcadfb8c290b6d4c9be515
kacy hulme <kacyhulme@gmail.com>
2014-05-09 17:36:44 -0500

    address display name method added

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,8 @@ class Address < ActiveRecord::Base
 
 	validates :addressable_type, :addressable_id, :street_number, :street_name, 
 						:city, :state, :zip, :phone, :website, presence: true
+	
+	def address_display_name #format address display for form
+		"#{street_number}" + " " +"#{street_name}" + " " + "#{zip}"
+	end
 end
