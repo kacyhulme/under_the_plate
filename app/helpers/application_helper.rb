e12c5c03b442a5f13c70a1be2d24f5fa842478fc
kacy hulme <kacyhulme@gmail.com>
2014-05-14 10:10:52 -0500

    adds content_for to title, now shows establishment name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper	
+	def page_title
+		if content_for?(:title)
+			"#{params[:controller].capitalize} - #{content_for(:title)}"
+		else
+			"#{params[:controller].capitalize}"
+		end
+	end
 end
+
