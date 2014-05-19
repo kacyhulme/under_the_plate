12f110134a3f606ccbb4ad8266f415c31b78d68c
kacy hulme <kacyhulme@gmail.com>
2014-05-19 15:02:46 -0500

    add custom title to view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,15 @@ module ApplicationHelper
 	def page_title
 		if content_for?(:title)
 			"#{params[:controller].capitalize} - #{content_for(:title)}"
+		elsif custom_title 
+			custom_title
 		else
 			"#{params[:controller].capitalize}"
 		end
+	end
+
+	def custom_title
+		"Under the Plate"
 	end
 end
 
