0f230c2e070991d55a40a0fa650b7714b453e16f
kacy hulme <kacyhulme@gmail.com>
2014-05-24 14:58:05 -0500

    button change to home nav bar and routing to land on establishments path instead of user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 		if user = User.authenticate(params[:email], params[:password])
 			session[:user_id] = user.id
 			flash[:notice] = "Welcome back #{user.name}!"
-			redirect_to session[:intended_url] || user
+			redirect_to session[:intended_url] || establishments_path
 			session[:intended_url] = nil
 		else
 			flash.now[:alert] = "Invalid password and/or email"	
