14f474cd1613ba4ff53ea9da03e1b25e8c66c234
kacy hulme <kacyhulme@gmail.com>
2014-05-30 23:12:33 -0500

    uses jquery to add background color change to thumbnails

--- app/assets/javascripts/establishments.js
+++ app/assets/javascripts/establishments.js
@@ -1,3 +1,9 @@
+
 $(document).ready(function(){
 	$(".flash").fadeOut(5000)
+	$(".thumbnail").on("mouseenter", function() {
+		$(this).addClass('highlight');
+		}).on("mouseleave", function() {
+		$(this).removeClass('highlight');
+	});
 });
