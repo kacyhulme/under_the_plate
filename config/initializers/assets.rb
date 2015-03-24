f9b3297b102b0749e43247034d222f39397b45a9
kacy hulme <kacyhulme@gmail.com>
2015-03-24 14:41:39 -0500

    add precompile assets to asset initializer file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,4 @@
 Rails.application.config.assets.precompile += %w( ckeditor/* )
+Rails.application.config.assets.precompile += %w( monologue/foundation_icons/social_foundicons.eot )
+Rails.application.config.assets.precompile += %w( monologue/foundation_icons/social_foundicons.woff )
+Rails.application.config.assets.precompile += %w( monologue/foundation_icons/social_foundicons.ttf )
