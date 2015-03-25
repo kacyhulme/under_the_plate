7547045dec4dc93b6223f73ac279f415ea6ee718
kacy hulme <kacyhulme@gmail.com>
2015-03-25 14:33:46 -0500

    add title tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,14 @@
 module ApplicationHelper	
 	def page_title
 		if content_for?(:title)
-			"#{params[:controller].capitalize} - #{content_for(:title)}"
-		elsif custom_title 
-			custom_title
+			"Healthy For Real - #{content_for(:title)}"
 		else
-			"#{params[:controller].capitalize}"
+			"Healthy For Real"
 		end
 	end
 
-	def custom_title
-		"Under the Plate"
-	end
+	# def custom_title
+	# 	"Under the Plate"
+	# end
 end
 
