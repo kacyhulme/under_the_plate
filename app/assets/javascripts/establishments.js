02b319a40ea638345076ae52d09362a1fcd0cd03
kacy hulme <kacyhulme@gmail.com>
2015-04-02 10:42:52 -0500

    put flash in separate file and added js so it only shows on controller/action

--- app/assets/javascripts/establishments.js
+++ app/assets/javascripts/establishments.js
@@ -1,9 +1,11 @@
-
 $(document).ready(function(){
-	$(".flash").fadeOut(5000)
 	$(".thumbnail").on("mouseenter", function() {
 		$(this).addClass('highlight');
-		}).on("mouseleave", function() {
-		$(this).removeClass('highlight');
-	});
+  }).on("mouseleave", function() {
+    $(this).removeClass('highlight');
+  });
+});
+
+$('.establishments.index').ready(function() {
+  $(".flash").fadeOut(5000)
 });
