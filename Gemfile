e3aa39a68bf7a72f53af29e6cbaf20a7b72e9172
kacy hulme <kacyhulme@gmail.com>
2015-04-05 15:29:23 -0500

    remove monologue gem

--- Gemfile
+++ Gemfile
@@ -52,5 +52,4 @@ gem 'friendly_id', '~> 5.0.0', :require => "friendly_id"
 gem 'geocoder'
 #gem 'gmaps4rails', '2.0.3'
 
-gem 'monologue', github: 'kacyhulme/monologue'
 gem 'non-stupid-digest-assets', '~> 1.0.4'
--- config/initializers/assets.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.assets.precompile += %w( ckeditor/* )
-Rails.application.config.assets.precompile += %w( monologue/foundation_icons/social_foundicons.eot )
-Rails.application.config.assets.precompile += %w( monologue/foundation_icons/social_foundicons.woff )
-Rails.application.config.assets.precompile += %w( monologue/foundation_icons/social_foundicons.ttf )
